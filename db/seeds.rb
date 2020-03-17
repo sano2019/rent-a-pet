b6309ed8a41d1c32441d80a171c4ead02bf79110
Madeline Andrean <madelineandrean@gmail.com>
2020-03-17 18:28:24 +0100

    created pets controller and index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# pets_attributes = [
+#   {
+#     pet_category: 'dog',
+#   },
+#   {
+#     pet_category: 'cat',
+#   }
+# ]
+# Pet.create!(pets_attributes)
