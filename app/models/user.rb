28c1b2deaa4c10fcb7b6a77347a934c6343a712c
Sander Nobel <53917461+sano2019@users.noreply.github.com>
2020-03-17 11:28:05 +0100

    Revert "Useless branch"

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_many :pets
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
