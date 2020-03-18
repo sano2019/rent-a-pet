0262238aeca312e6cf421d647f9c3c917b7040b3
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-18 17:59:57 +0100

    create the create action in reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,13 +2,21 @@ class ReservationsController < ApplicationController
   def new
     @pet = Pet.find(params[:pet_id])
     @reservation = Reservation.new
+  end
 
   def create
+    @pet = Pet.find(params[:pet_id])
     @reservation = Reservation.new(reserv_params)
+    @reservation.user = current_user
+    @reservation.pet = @pet
+    # still to implement: the price
+    # period = @reservation.end_date - @reservation.start_date
+    # @reservation.total_price = @pet.price_per_day * period
   end
 
   private
 
   def reserv_params
+    params.require(:reservation).permit(:start_date, :end_date)
   end
 end
