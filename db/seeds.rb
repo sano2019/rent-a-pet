11bafdf3d551eb7260d71d830fc595c973b40d3a
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-19 15:59:00 +0100

    crash test with 2 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,15 @@ User.destroy_all
 Pet.destroy_all
 puts "Creating users"
 User.create!(email: "woland@sisi.com", password: "123456")
+User.create!(email: "hehe@sisi.com", password: "123456")
 puts "Creating pets"
 Pet.create!(name: "Sisi", user: User.first, pet_category: "cat", age: 4, description: "playful", price_per_day: 20, )
 Pet.create!(name: "Rex", user: User.first, pet_category: "dog", age: 1, description: "young and playful", price_per_day: 100, )
+Pet.create!(name: "Rex", user: User.last, pet_category: "cat", age: 3, description: "young and playful", price_per_day: 50, )
+Pet.create!(name: "Rex", user: User.last, pet_category: "dog", age: 5, description: "dangerous", price_per_day: 10, )
 puts "Creating reservations"
 Reservation.create!(start_date: DateTime.parse("10/03/2020"), end_date: DateTime.parse("20/03/2020"),
  user_id: User.first.id,
- pet_id: Pet.first.id,
+ pet_id: Pet.last.id,
  total_price: 20000)
 puts "Done!!!!!"
