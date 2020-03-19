7c6cc564640a3ef191d0f819b289c6694912c22e
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-19 17:49:38 +0100

    fixed the create a reservation from the fostering part

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -16,6 +16,10 @@ class PetsController < ApplicationController
   def create
       @pet = Pet.new(pet_params)
       @pet.user = current_user
+      period = @pet.end_date - @pet.start_date
+       # turn it into an integer because it was a rational
+      period = period.to_i
+      @pet.total_price = (@pet.price_per_day) * period
       if @pet.save
         redirect_to @pet, notice: 'successfully created.'
       else
@@ -36,7 +40,7 @@ class PetsController < ApplicationController
   private
 
   def pet_params
-      params.require(:pet).permit(:name, :age, :price_per_day, :description, :pet_category, :user)
+      params.require(:pet).permit(:name, :age, :price_per_day, :description, :pet_category, :user, :start_date, :end_date)
   end
 
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,4 +3,10 @@ class Pet < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
   validates :name, presence: true
+
+
+  def full_price
+    # return the total_price / 100 (because we are storing cents)
+    total_price / (100)
+  end
 end
