8be82662de631859c43668b9b6391815ccc900a0
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-19 16:45:46 +0100

    resolved the seeding according with new schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,15 @@ puts "Creating users"
 User.create!(email: "woland@sisi.com", password: "123456")
 User.create!(email: "hehe@sisi.com", password: "123456")
 puts "Creating pets"
-Pet.create!(name: "Sisi", user: User.first, pet_category: "cat", age: 4, description: "playful", price_per_day: 20, )
-Pet.create!(name: "Rex", user: User.first, pet_category: "dog", age: 1, description: "young and playful", price_per_day: 100, )
-Pet.create!(name: "Rex", user: User.last, pet_category: "cat", age: 3, description: "young and playful", price_per_day: 50, )
-Pet.create!(name: "Rex", user: User.last, pet_category: "dog", age: 5, description: "dangerous", price_per_day: 10, )
+Pet.create!(name: "Sisi", user: User.first, pet_category: "cat", age: 4, description: "playful", price_per_day: 20,
+start_date: DateTime.parse("10/03/2020"), end_date: DateTime.parse("20/03/2020"), total_price: 20000 )
+Pet.create!(name: "Rex", user: User.first, pet_category: "dog", age: 1, description: "young and playful", price_per_day: 100,
+  start_date: DateTime.parse("05/04/2020"), end_date: DateTime.parse("15/04/2020"), total_price: 100000)
+Pet.create!(name: "Rex", user: User.last, pet_category: "cat", age: 3, description: "young and playful", price_per_day: 50,
+  start_date: DateTime.parse("10/05/2020"), end_date: DateTime.parse("15/05/2020"), total_price: 10000)
+Pet.create!(name: "Rex", user: User.last, pet_category: "dog", age: 5, description: "dangerous", price_per_day: 10,
+start_date: DateTime.parse("8/06/2020"), end_date: DateTime.parse("28/06/2020"), total_price: 10000 )
 puts "Creating reservations"
-Reservation.create!(start_date: DateTime.parse("10/03/2020"), end_date: DateTime.parse("20/03/2020"),
- user_id: User.first.id,
- pet_id: Pet.last.id,
- total_price: 20000)
+Reservation.create!( user_id: User.first.id, pet_id: Pet.last.id)
+Reservation.create!( user_id: User.last.id, pet_id: Pet.first.id)
 puts "Done!!!!!"
