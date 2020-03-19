d4dc7f6fab029f5fd2cfee01a04ab0447a970064
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-19 18:36:24 +0100

    crash test the reservation journey (show, offer to foster)

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,29 @@
 
 ActiveRecord::Schema.define(version: 2020_03_19_152717) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "pets", force: :cascade do |t|
     t.text "pet_category"
@@ -28,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_152717) do
     t.date "start_date"
     t.date "end_date"
     t.integer "total_price"
+    t.string "image"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
@@ -52,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_152717) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "pets", "users"
   add_foreign_key "reservations", "pets"
   add_foreign_key "reservations", "users"
