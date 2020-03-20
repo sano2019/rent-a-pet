123e94122e212b1a3539842243827592d01cefde
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-20 16:42:15 +0100

    fixed if no current_user for home page display pet cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    if current_user.pets.any?
-
-      @pets = Pet.where.not(user: current_user)
+    if user_signed_in?
+      if current_user.pets.any?
+        @pets = Pet.where.not(user: current_user)
+      else
+        @pets = Pet.all
+      end
     else
       @pets = Pet.all
     end
