39b5a17d910d26e97c88879f9dde3b3d9bf6d728
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-20 12:31:25 +0100

    adding reservation_status to Reservations table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_03_19_152717) do
-
+ActiveRecord::Schema.define(version: 2020_03_20_112718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_152717) do
     t.bigint "pet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reservation_status", default: "pending"
     t.index ["pet_id"], name: "index_reservations_on_pet_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
