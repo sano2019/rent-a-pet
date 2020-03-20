0c21c67f45037e89c4320f157bbeb3dd019eb3b7
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-03-20 16:35:40 +0100

    fixed the home: owner can see only pets from others

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    if current_user.pets.any?
+
+      @pets = Pet.where.not(user: current_user)
+    else
+      @pets = Pet.all
+    end
   end
 end
